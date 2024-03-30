2188eb8d8f66a1bdbce2ba0aa7219389c1b193f4
junil <junilyang.dev@gmail.com>
2024-03-30 19:44:53 +0900

    #6.2 Deps (12:03)

--- src/App.js
+++ src/App.js
@@ -2,15 +2,24 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import {useState, useEffect} from "react";
 
+
 function App() {
     const [counter, setValue] = useState(0);
+    const [keyword, setKeyword] = useState("");
     const onClick = () => setValue((prev)=> prev+1);
-    console.log("i run all the time");
+    const onChange = (event) => setKeyword(event.target.value);
     useEffect(() => {
-        console.log("CALL THE API..");
+        console.log("I run only once.");
     }, []);
+    useEffect(() => {
+        console.log("I run when 'keyword' changes,Search for", keyword);
+    }, [keyword]);
+    useEffect(() => {
+        console.log("I run when 'counter' changes,Search for", counter);
+    }, [counter]);
     return (
         <div>
+            <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
             <h1 className={styles.title}>초기 세팅</h1>
             <Button text={"Continue"}/>
             <h2>{counter}</h2>
