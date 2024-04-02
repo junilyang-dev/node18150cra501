21eeaa98c792df8157cb6fcfe6256dbef4d2151e
junil <junilyang.dev@gmail.com>
2024-04-02 20:22:39 +0900

    list_movies fetch api async-await json

--- src/App.js
+++ src/App.js
@@ -4,10 +4,9 @@ function App() {
     const [loading, setLoading] = useState(true);
     const [movies,setMovies] = useState([]);
     const getMpvies = async () => {
-        const response = await fetch(
+        const json = await (await fetch(
             `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year&language=ko-KR&page=1`
-        );
-        const json = await response.json();
+        )).json();
         setMovies(json.data.movies);
         setLoading(false);
     }
