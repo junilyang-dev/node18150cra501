b8e061fc3ebfad93a383b441b02d08f2ac2c1030
junil <junilyang.dev@gmail.com>
2024-04-03 23:23:43 +0900

    Movie component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import {useEffect, useState} from "react";
+import Movie from "./Movie";
 
 function App() {
     const [loading, setLoading] = useState(true);
@@ -17,17 +18,13 @@ function App() {
     return (
         <div>
             { loading ? <h1>Loading...</h1> : <div>{movies.map((movie) =>(
-                <div key={movie.id}>
-                    <img src={movie.medium_cover_image} />
-                    <h2>{movie.title}</h2>
-                    <p>{movie.summary}</p>
-                    <ul>
-                        {movie.genres.map(g => (
-                            <li key={g}>{g}</li>)
-                        )
-                        }
-                    </ul>
-                </div>
+                <Movie
+                    key={movie.id}
+                    coverImg={movie.medium_cover_image}
+                    title={movie.title}
+                    summary={movie.summary}
+                    genres={movie.genres}
+                />
                 )
             )}</div> }
         </div>
