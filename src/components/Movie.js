bcbb3a3ea6defae60206f4b70e4303c902f2d08b
junil <junilyang.dev@gmail.com>
2024-04-07 19:56:00 +0900

    summary ... slice

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,14 +2,15 @@ import PropTypes from "prop-types";
 import {Link} from "react-router-dom";
 import {useState} from "react";
 
-function Movie({id, coverImg, title, summary, genres}) {
+function Movie({id, coverImg, title, year, summary, genres}) {
     return (
         <div>
             <img src={coverImg} alt={title}/>
             <h2>
                 <Link to={`/movie/${id}`}>{title}</Link>
             </h2>
-            <p>{summary}</p>
+            <h3>{year}</h3>
+            <p>{summary.length > 235 ? `${summary.slice(0,235)}...` : summary}</p>
             <ul>
                 {genres.map(g => (
                         <li key={g}>{g}</li>
